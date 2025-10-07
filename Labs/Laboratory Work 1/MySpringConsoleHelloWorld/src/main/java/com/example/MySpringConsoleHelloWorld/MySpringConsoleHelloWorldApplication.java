14aea24bc79cf8d9e35317f759cfeb22edf04a39
ArtKud <kudryavtsev.artem@lll.kpi.ua>
2025-10-07 22:37:51 +0200

    I've added MySpringConsoleHelloWorldApplication

---
+++ Labs/Laboratory Work 1/MySpringConsoleHelloWorld/src/main/java/com/example/MySpringConsoleHelloWorld/MySpringConsoleHelloWorldApplication.java
@@ -0,0 +1,18 @@
+package com.example.MySpringConsoleHelloWorld;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringConsoleHelloWorldApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringConsoleHelloWorldApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello, Spring Console World! ");
+    }
+}
